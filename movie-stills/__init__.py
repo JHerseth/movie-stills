1b64118ea1d85431c11941d63c6f51424acfcb42
Jonas Herseth <jonas.herseth@gmail.com>
2020-09-24 12:38:01 +0200

    added progressbar

--- movie-stills/__init__.py
+++ movie-stills/__init__.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 from pathlib import Path
 import os
+import progressbar
 
 parser = argparse.ArgumentParser(description="Summarizes a movie in screenshots.")
 parser.add_argument('-f','--file',      type=str,               help="Full path to file to summarize.")
@@ -36,26 +37,26 @@ start_time = 60 * 3 # the first 3 minutes are usually just intros
 tail = 60 * 10 # Skip credits 
 end_time = int(movie_data['format']['duration'].split('.')[0]) - tail
 
-current_time = 0
+current_time = start_time
 count = 0
 
-while True:
-    if count == 0:
-        current_time = start_time
-    else:
-        current_time = current_time + (args.step + (randrange(-1, 2) * randrange(1, args.variance)))
+timestamps = []
 
-    if current_time >= end_time:
-        break
-    img_file = p / f'img{count}.png'
-    try:
-        frame = (
-            ffmpeg
-            .input(args.file, ss=current_time)
-            .filter('scale', args.width, -1)
-            .output(str(img_file.absolute()), vframes=1, loglevel='warning')
-            .run()
-            )
-    except ffmpeg.Error as e:
-        sys.exit()
-    count = count + 1
+while current_time <= end_time:
+    current_time = current_time + (args.step + (randrange(-1, 2) * randrange(1, args.variance)))
+    timestamps.append(current_time)
+
+with progressbar.ProgressBar(max_value=len(timestamps)) as bar:
+    for count, timestamp in enumerate(timestamps):
+        img_file = p / f'img{count}.png'
+        try:
+            frame = (
+                ffmpeg
+                .input(args.file, ss=timestamp)
+                .filter('scale', args.width, -1)
+                .output(str(img_file.absolute()), vframes=1, loglevel='fatal')
+                .run()
+                )
+        except ffmpeg.Error as e:
+            sys.exit()
+        bar.update(count)
